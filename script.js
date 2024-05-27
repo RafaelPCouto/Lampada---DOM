2e79ee5753c8dfcd361fa63c5ceb86e401777667
Rafael Couto <rafaelcouto58@gmail.com>
2024-05-27 14:33:28 -0300

    Implementing arrow functions to change images.

---
+++ script.js
@@ -0,0 +1,7 @@
+let lamp = document.getElementById('lamp')
+
+const turnOn = () => lamp.src = "images/on.svg";
+
+const turnOff = () => lamp.src = 'images/off.svg';
+
+const lampBreak = () => lamp.src = 'images/broke.svg'
